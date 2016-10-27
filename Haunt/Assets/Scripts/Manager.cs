f9b42cc6110b6aa88dee2bb726a251a2996e449c
williambl <william@williambl.com>
2016-10-27 23:19:55 +0100

    Added win text

--- Haunt/Assets/Scripts/Manager.cs
+++ Haunt/Assets/Scripts/Manager.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Manager : MonoBehaviour {
 	
 	public bool won;
+	public GameObject text;
 
 	// Use this for initialization
 	void Start () {
@@ -13,7 +14,7 @@ public class Manager : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (won) {
-			Debug.Log ("won");
+			text.SetActive (true);
 		}
 	}
 }
