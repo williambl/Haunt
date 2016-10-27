0d38dfd3848e6953e2ac55431ddc051d6b5ce4bd
williambl <william@williambl.com>
2016-10-27 19:50:21 +0100

    Made a door

---
+++ Haunt/Assets/Scripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.layer == 10) 
+		{
+			transform.position += new Vector3 (0, 5, 0);
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.layer == 10) 
+		{
+			transform.position -= new Vector3 (0, 5, 0);
+		}
+	}
+}
