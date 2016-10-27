e8e8d123d598bfd40b1b7893d9c67c6820507ce8
williambl <william@williambl.com>
2016-10-27 23:16:01 +0100

    Added winning

---
+++ Haunt/Assets/Scripts/Manager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+	
+	public bool won;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (won) {
+			Debug.Log ("won");
+		}
+	}
+}
---
+++ Haunt/Assets/Scripts/Objective.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objective : MonoBehaviour {
+
+	Manager manager;
+
+	// Use this for initialization
+	void Start () {
+		manager = FindObjectOfType<Manager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Player") {
+			manager.won = true;
+		}
+	}
+}
