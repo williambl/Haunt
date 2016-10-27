cc5d783caece4f3fdf60657c8063c6a78803d4cd
williambl <william@williambl.com>
2016-10-27 22:43:30 +0100

    Made door move more smoothly

--- Haunt/Assets/Scripts/Door.cs
+++ Haunt/Assets/Scripts/Door.cs
@@ -3,13 +3,24 @@ using System.Collections;
 
 public class Door : MonoBehaviour {
 
+	bool isGoingUp = false; //Up = True, Down = False
+	public float speed = 0.1f;
+	public float top = 2.75f;
+	public float bottom = -3.75f;
+
+
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
+		if (isGoingUp && transform.position.y < top) {
+			transform.position += new Vector3 (0, speed, 0);
+		} else if (!isGoingUp && transform.position.y > bottom) {
+			transform.position -= new Vector3 (0, speed, 0);
+		}
 	
 	}
 
@@ -17,7 +28,7 @@ public class Door : MonoBehaviour {
 	{
 		if (other.gameObject.layer == 10) 
 		{
-			transform.position += new Vector3 (0, 5, 0);
+			isGoingUp = true;
 		}
 	}
 
@@ -25,7 +36,7 @@ public class Door : MonoBehaviour {
 	{
 		if (other.gameObject.layer == 10) 
 		{
-			transform.position -= new Vector3 (0, 5, 0);
+			isGoingUp = false;
 		}
 	}
 }
