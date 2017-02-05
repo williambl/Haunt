f1cda8c6008a95c98184b3e415e8c8da2f138b54
williambl <william@williambl.com>
2017-02-05 16:21:08 +0000

    Added an objective to the second level

---
+++ Haunt/Assets/Scripts/Items/Objectives/Objective1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objective1 : MonoBehaviour {
+
+	GameManager manager;
+	ItemComponent itemC;
+	Rigidbody rigid;
+	Collider coll;
+
+	void Start () {
+		itemC = GetComponent<ItemComponent> ();
+		itemC.item = new Item ("objective1", gameObject);
+		Registry.RegisterItem (itemC.item);
+		rigid = GetComponent<Rigidbody> ();
+		coll = GetComponent<Collider> ();
+		manager = GameObject.Find ("Manager").GetComponent<GameManager> ();
+	}
+
+	void Update () {
+		rigid.isKinematic = itemC.isHeld;
+		coll.enabled = !itemC.isHeld;
+
+		if (itemC.isHeld) {
+			manager.won = true;
+		}
+	}
+}
