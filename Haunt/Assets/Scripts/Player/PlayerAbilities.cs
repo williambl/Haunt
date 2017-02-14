36065d0e7f215aed10e4bd5b35d768ed67392ea8
williambl <william@williambl.com>
2017-02-14 12:25:43 +0000

    Sorted player scripts into folder
    Also renamed abilities to playerabilities

--- Haunt/Assets/Scripts/Player/PlayerAbilities.cs
+++ Haunt/Assets/Scripts/Player/PlayerAbilities.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Abilities : MonoBehaviour {
+public class PlayerAbilities : MonoBehaviour {
 
 	public bool desolidify;
 	public bool possess;
