ef5ecfd7fe535a0ac8f749161c90eca3decca7d9
williambl <william@williambl.com>
2017-04-03 19:49:37 +0100

    Continued work on locks

---
+++ Haunt/Assets/Scripts/Enviroment/MultipleLockDoor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleLockDoor : Door {
+
+	public List<Lock> locks;
+
+	public void CheckLocks () {
+		int count = 0;
+
+		foreach (Lock l in locks) {
+			if (l.isLocked)
+				count++;
+		}
+		if (count == locks.Count)
+			isGoingUp = true;
+		else
+			isGoingUp = false;
+	}
+}
