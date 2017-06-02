4d9eca94b5c2ea843bc2a5feb4adf7106b7c1a82
williambl <william@williambl.com>
2017-06-02 17:17:39 +0100

    Added ItemExistsFromID and ItemExists
    Better way of determining whether an item exists in the registry or
    not. ItemExistsFromID is now used when registering items to prevent
    name clashes.
    
    I don’t see a point in adding FindItem(Item). It would just return what
    you put in.

--- Haunt/Assets/Scripts/Items/Registry.cs
+++ Haunt/Assets/Scripts/Items/Registry.cs
@@ -12,17 +12,37 @@ public class Registry {
 	/// <param name="item">Item to register.</param>
 	public static void RegisterItem (Item item)
 	{
-		if (!itemRegistry.Contains (item))
+		if (ItemExistsFromID (item.id))
 			itemRegistry.Add (item);
 	}
-
+		
 	/// <summary>
-	/// Finds an item by its id.
+	/// Finds an item from its ID.
 	/// </summary>
 	/// <returns>The item.</returns>
-	/// <param id="id">The id of the item.</param>
+	/// <param id="id">ID of the item.</param>
 	public static Item FindItemByID (string id)
 	{
 		return itemRegistry.Find (x => x.id == id);
+	}
+
+	/// <summary>
+	/// Determines if an item exists in the registry from its ID.
+	/// </summary>
+	/// <returns><c>true</c>, if the item exists, <c>false</c> otherwise.</returns>
+	/// <param name="id">ID of the item.</param>
+	public static bool ItemExistsFromID (string id)
+	{
+		return itemRegistry.Exists (x => x.id == id);
+	}
+
+	/// <summary>
+	/// Determines whether an item exists in the registry.
+	/// </summary>
+	/// <returns><c>true</c>, if the item exists, <c>false</c> otherwise.</returns>
+	/// <param name="item">The item.</param>
+	public static bool ItemExists (Item item)
+	{
+		return itemRegistry.Exists (x => x == item);
 	}
 }
