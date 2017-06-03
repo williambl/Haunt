af56ea57bea3ecb3636c57d0b36215d0fe28eb4d
williambl <william@williambl.com>
2017-06-03 13:16:30 +0100

    Projectile now explodes on contact

--- Haunt/Assets/Scripts/Projectile.cs
+++ Haunt/Assets/Scripts/Projectile.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour {
 
+	ParticleSystem ps;
+
+	bool isExploding = false;
+
+	void Start ()
+	{
+		ps = GetComponent<ParticleSystem> ();
+		ps.Stop ();
+	}
+
+
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.layer == 10) {
@@ -14,6 +25,19 @@ public class Projectile : MonoBehaviour {
 		if (collision.gameObject.tag == "Lock") {
 			collision.gameObject.GetComponent<Lock> ().ToggleLock ();
 		}
-		Destroy (gameObject);
+		ps.Play ();
+		isExploding = true;
+		GetComponent<Rigidbody> ().velocity = Vector3.zero;
+		GetComponent<TrailRenderer> ().enabled = false;
+		GetComponent<Renderer> ().enabled = false;
+	}
+
+	void Update ()
+	{
+		if (isExploding) {
+			if (!ps.IsAlive ()) {
+				Destroy (gameObject);
+			}
+		}
 	}
 }
