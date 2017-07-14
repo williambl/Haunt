83c93bc6875749ce3945c5e30b1a9221ea7ac789
williambl <william@williambl.com>
2017-07-14 13:48:09 +0100

    Started working on a rotating door script

---
+++ Haunt/Assets/Scripts/Environment/Door/RotatingDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingDoor : MonoBehaviour {
+
+	[Header("Door Properties")]
+	public bool isOpening = false; //Up = True, Down = False
+	public float speed = 0.1f;
+	public float open = 90f;
+	public float closed = 0f;
+    public float currentrotation;
+    public Vector3 rotateoffset;
+
+    private Vector3 rotatepoint;
+
+	public void Update ()
+	{
+    	Move ();
+		rotatepoint = transform.position + rotateoffset;
+	}
+
+	public void Move ()
+	{
+		if (isOpening && currentrotation < open) {
+			currentrotation += speed;
+			transform.RotateAround(rotatepoint, Vector3.up, currentrotation);;
+		} else if (!isOpening && currentrotation > closed) {
+			currentrotation -= speed;
+			transform.RotateAround(rotatepoint, Vector3.up, currentrotation);
+		}
+	}
+}
