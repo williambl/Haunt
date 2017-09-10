9eca02a17a7e22e4bebed0e1cb28adbdd470e20a
williambl <william@williambl.com>
2017-09-10 10:55:53 +0100

    Started a minigame at the end of level 6. For now you win as soon as you shoot one lock, but once it's finished you'll have to hit multiple at the right times...

---
+++ Haunt/Assets/Scripts/Level6MinigameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level6MinigameManager : MonoBehaviour, ILocked {
+
+        public List<Lock> locks;
+        public List<bool> lockBools;
+        public bool hasStarted = false;
+        GameManager manager;
+        
+	// Use this for initialization
+	public new void Start () {
+            manager = GameObject.FindGameObjectWithTag("Manager").GetComponent<GameManager> ();
+	    foreach (Lock l in locks) {
+                l.AddLockedObject(this);
+            }	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            if (hasStarted)
+                manager.won = true;
+	}
+
+        public void CheckLocks ()
+        {
+            	lockBools.Clear ();
+		foreach (Lock l in locks)
+			lockBools.Add (l.isLocked);
+
+		if (lockBools.Contains (false))
+			hasStarted = true;
+		else
+			hasStarted = false;
+        }
+}
